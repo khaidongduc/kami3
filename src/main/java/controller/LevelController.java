03f0cc81d1e5d454776b1a132afeccdaf25e4fe1
Khai Dong <dongk@union.edu>
2023-01-27 18:52:59 -0500

    alert when wrong argument is given

--- src/main/java/controller/LevelController.java
+++ src/main/java/controller/LevelController.java
@@ -1,13 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import model.Level;
+import model.Move;
 
 public class LevelController {
 
-    private Level level;
+    private final Level level;
 
     public LevelController(Level level){
         this.level = level;
@@ -15,9 +17,14 @@ public class LevelController {
 
 
     public void handleGridClickEvent(ActionEvent actionEvent) {
-        Button targetButton = (Button) actionEvent.getTarget();
-        System.out.print(GridPane.getRowIndex(targetButton));
-        System.out.print(" ");
-        System.out.println(GridPane.getColumnIndex(targetButton));
+        try {
+            Button targetButton = (Button) actionEvent.getTarget();
+            int row = GridPane.getRowIndex(targetButton);
+            int col = GridPane.getColumnIndex(targetButton);
+            level.play(new Move(level.getCurrentColor(), row, col));
+        } catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.toString());
+            alert.show();
+        }
     }
 }
