91f7507f34ccd9c748ff4d0396752a42ed6244f1
Zach <dubinskz@union.edu>
2023-01-27 15:32:42 -0500

    Our first window

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,16 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
-class App extends Application {
+public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-      /*
-      Code for JavaFX application.
-      (Stage, scene, scene graph)
-      */
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("LevelView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
-    public static void main(String[] args){
-        launch(args);
-    }
+    public static void main(String[] args){launch(args);}
 }
