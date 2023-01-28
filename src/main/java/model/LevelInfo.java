803316144319f00df19ca901830b81a0c165301a
Khai Dong <dongk@union.edu>
2023-01-28 09:59:42 -0500

    create levelInfo model to only send out certain parts of a level

---
+++ src/main/java/model/LevelInfo.java
@@ -0,0 +1,14 @@
+package model;
+
+public class LevelInfo {
+    private final int levelId;
+
+    public LevelInfo(int levelId){
+        this.levelId = levelId;
+    }
+
+    public int getLevelId(){
+        return levelId;
+    }
+
+}
