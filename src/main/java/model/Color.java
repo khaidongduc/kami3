883bab11df1343d3febc2a0f6b5ca47c38d9d40a
Khai Dong <dongk@union.edu>
2023-01-28 10:39:46 -0500

    comment code

--- src/main/java/model/Color.java
+++ src/main/java/model/Color.java
@@ -2,16 +2,31 @@ package model;
 
 import java.util.Objects;
 
+/**
+ * a basic RGB color model
+ *
+ * @author Khai Dong
+ */
 public class Color {
+
+    private static final int MAX_COLOR_VALUE = 255;
+
     private int colorId;
     private final int rValue;
     private final int gValue;
     private final int bValue;
 
+    /**
+     * constructor
+     * create a new Color with RGB values
+     * @param rValue red value
+     * @param gValue green value
+     * @param bValue blue value
+     */
     public Color(int rValue, int gValue, int bValue){
-        if(0 <= rValue && rValue < 256 
-        && 0 <= gValue && gValue < 256 
-        && 0 <= bValue && bValue < 256){
+        if(0 <= rValue && rValue <= MAX_COLOR_VALUE
+        && 0 <= gValue && gValue <= MAX_COLOR_VALUE
+        && 0 <= bValue && bValue <= MAX_COLOR_VALUE){
             this.rValue = rValue;
             this.gValue = gValue;
             this.bValue = bValue;
@@ -20,22 +35,42 @@ public class Color {
         }
     }
 
+    /**
+     * get the colorId
+     * colorId is assigned by the ColorRepository
+     * @return the color id
+     */
     public int getColorId(){
         return colorId;
     }
 
+    /**
+     * set the colorId
+     * only to be use with ColorRepository
+     * package-private
+     * @param colorId the new colorId
+     */
     void setColorId(int colorId){
         this.colorId = colorId;
     }
 
+    /**
+     * @return the red value of the color
+     */
     public int getRValue(){
         return rValue;
     }
 
+    /**
+     * @return the green value of the color
+     */
     public int getGValue(){
         return gValue;
     }
 
+    /**
+     * @return the blue value of the color
+     */
     public int getBValue(){
         return bValue;
     }
