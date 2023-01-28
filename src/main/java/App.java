15e37034b1dfd5f57359cbe687087425327b77be
Khai Dong <dongk@union.edu>
2023-01-28 01:54:39 -0500

    fix app set up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,9 @@
 import controller.LevelController;
+import controller.ViewSwitcher;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 import model.Level;
 import model.LevelImpl;
 import view.LevelView;
@@ -11,12 +12,20 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+
+        ViewSwitcher.setStage(stage);
+
         Level level = new LevelImpl(1);
         LevelController controller = new LevelController(level);
         LevelView view = new LevelView(controller, level);
+        ViewSwitcher.addView("Level1", view);
 
-        Scene scene = new Scene(view.asParent(), 400, 400);
-        stage.setScene(scene);
+        Level level2 = new LevelImpl(3);
+        LevelController controller2 = new LevelController(level2);
+        LevelView view2 = new LevelView(controller2, level2);
+        ViewSwitcher.addView("Level2", view2);
+
+        stage.setScene(view.getScene());
         stage.show();
     }
     public static void main(String[] args){launch(args);}
