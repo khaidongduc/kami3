58c5ca042232817868342cdb9582ccc541e2b915
Khai Dong <dongk@union.edu>
2023-01-28 10:25:28 -0500

    make levelRepository Observable (for later when saving level)

--- src/main/java/model/LevelRepository.java
+++ src/main/java/model/LevelRepository.java
@@ -1,8 +1,13 @@
 package model;
 
+import utils.Observable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class LevelRepository {
+public class LevelRepository extends Observable {
     private static LevelRepository instance;
 
 
@@ -10,19 +15,28 @@ public class LevelRepository {
 
     }
 
-    public LevelRepository getInstance(){
+    public static LevelRepository getInstance(){
         if(instance == null)
             instance = new LevelRepository();
         return instance;
     }
 
-    public Level getGame(int gameId){
+    public Level getLevel(int gameId){
         return null;
     }
 
-    public List<Level> listGames(){
-        return null;
+    public void saveLevel(Level level){
+        // TODO: LevelBuilder
+        notifyObservers();
     }
 
+    public List<LevelInfo> listLevelInfo(){
+        File folder = new File(getClass().getResource("levels").getPath());
+        LinkedList<LevelInfo> levelInfos = new LinkedList<>();
+        for(File file : folder.listFiles()){
+            levelInfos.add(new LevelInfo(Integer.parseInt(file.getName())));
+        }
+        return levelInfos;
+    }
 
 }
