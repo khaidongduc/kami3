ae7702ce21293a92aa57b664e027e8d1fe568db6
Khai Dong <dongk@union.edu>
2023-02-02 13:15:13 -0500

    create place holder for LevelSolver

--- src/main/java/service/LevelSolver.java
+++ src/main/java/service/LevelSolver.java
@@ -1,8 +1,19 @@
 package service;
 
+import model.ColorGrid;
+import model.Move;
+
+import java.util.List;
+
 /**
  * service class to solve a level
  * TODO: need implementation
  */
 public class LevelSolver {
+
+    public List<Move> solveColorGrid(ColorGrid grid)
+    {
+        return null;
+    }
+
 }
