4e736b91d40465f71c87c34d05264e0f1eeab52c
Zach <dubinskz@union.edu>
2023-02-02 20:44:52 -0500

    Controller loads levels from files

--- src/main/java/model/LevelRepository.java
+++ src/main/java/model/LevelRepository.java
@@ -5,6 +5,7 @@ import utils.Observable;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * a level repository that manages all Levels (list and create)
@@ -32,15 +33,28 @@ public class LevelRepository extends Observable {
         return instance;
     }
 
-    /**
-     * get a Level by levelId
-     * @param levelId the levelId
-     * @return the Level generated
-     */
-    public Level getLevel(int levelId){
-        return new LevelImpl(levelId);
-    }
 
+    public LevelImpl loadLevel(int levelId){
+        try {
+            String relPath = String.format("levels/%s", levelId);
+            File file = new File(getClass().getResource(relPath).getPath());
+            Scanner scanner = new Scanner(file);
+            int numRows = scanner.nextInt();
+            int numCols = scanner.nextInt();
+            ColorGrid grid = new ColorGrid(numRows, numCols);
+            for (int i = 0; i < numRows; ++i){
+                for (int j = 0; j < numCols; ++j) {
+                    int colorId = scanner.nextInt();
+                    if(grid.getColorOfEntry(i, j) != colorId) grid.setColor(colorId, i, j);
+                }
+            }
+            int maxNumTurn = scanner.nextInt();
+            Color curColor = ColorRepository.getInstance().getColor(grid.getAvailableColorIds().stream().findFirst().get());
+            return new LevelImpl(grid, curColor, maxNumTurn, levelId);
+        } catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
     /**
      * assign an id and save the level into hard drive
      * @param level the level
