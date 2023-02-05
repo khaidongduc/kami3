fcb6fc20824e754ca2acbbc18522106690c4eea6
Khai Dong <dongk@union.edu>
2023-02-05 11:10:06 -0500

    add a config file

---
+++ src/main/java/edu/union/Config.java
@@ -0,0 +1,17 @@
+package edu.union;
+
+import edu.union.model.Color;
+
+public class Config {
+
+    public static final Integer DEFAULT_BUILDER_ROWS = 5;
+    public static final Integer DEFAULT_BUILDER_COLS = 5;
+
+    public static final Color[] DEFAULT_COLORS = new Color[]{
+            new Color(255, 0, 0),
+            new Color(0, 255, 0),
+            new Color(0, 0, 255),
+            new Color(0, 255, 255)
+    };
+
+}
