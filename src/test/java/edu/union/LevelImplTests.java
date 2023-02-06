0e952f33e9968e906ecdc9a71f37e4e79de055b3
Khai Dong <dongk@union.edu>
2023-02-06 13:03:16 -0500

    fix test

--- src/test/java/edu/union/LevelImplTests.java
+++ src/test/java/edu/union/LevelImplTests.java
@@ -3,6 +3,7 @@ package edu.union;
 import edu.union.model.Color;
 import edu.union.model.ColorGrid;
 import edu.union.model.LevelImpl;
+import edu.union.model.LevelInfo;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -18,13 +19,17 @@ public class LevelImplTests {
     private Color color;
 
     @Before
-    public void setUp(){level = new LevelImpl(grid, color, 2, 1);}
+    public void setUp(){
+        grid = new ColorGrid(5, 5);
+        level = new LevelImpl(grid, color, 3, new LevelInfo(1));
+    }
 
     @After
     public void tearDown(){level = null;}
 
     @Test
     public void testConstruct(){
+        System.out.println(level);
         assertEquals(5, level.getNumRows());
         assertEquals(5, level.getNumCols());
         assertEquals(3, level.numMoveRemaining());
