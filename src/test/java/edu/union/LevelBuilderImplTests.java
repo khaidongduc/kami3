cb2f019cb767e93c2b8fcf10d842efa78acac7e7
John Collins <collinsj@union.edu>
2023-02-07 14:53:15 -0500

    adding tests and finding some issues with the color ids that we should discuss

--- src/test/java/edu/union/LevelBuilderImplTests.java
+++ src/test/java/edu/union/LevelBuilderImplTests.java
@@ -1,2 +1,66 @@
-package edu.union;public class LevelBuilderImplTests {
+package edu.union;
+import edu.union.model.*;
+import edu.union.service.LevelRepository;
+import edu.union.service.RawTextLevelRepositoryStrategy;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class LevelBuilderImplTests {
+    private LevelBuilder builder;
+    private final Color red = new Color(255, 0, 0);
+    private final Color green = new Color(0, 255, 0);
+    private final Color blue = new Color(0, 0, 255);
+    private final Color light_blue = new Color(0, 255, 255);
+
+    @Before
+    public void setUp(){
+        builder = new LevelBuilderImpl(4, 6);
+    }
+
+    @After
+    public void tearDown(){builder = null;}
+
+    @Test
+    public void testLevelBuilder_Default(){
+        LevelBuilder levelBuilder = new LevelBuilderImpl();
+        assertEquals(5, levelBuilder.getNumCols());
+        assertEquals(5, levelBuilder.getNumRows());
+        List<Color> colors = levelBuilder.getColors();
+        assertTrue(colors.contains(red));
+        assertFalse(colors.contains(green));
+        assertFalse(colors.contains(blue));
+        assertFalse(colors.contains(light_blue));
+    }
+
+    @Test
+    public void testLevelBuilder(){
+        assertEquals(6, builder.getNumCols());
+        assertEquals(4, builder.getNumRows());
+        List<Color> colors = builder.getColors();
+        assertTrue(colors.contains(red));
+        assertFalse(colors.contains(green));
+        assertFalse(colors.contains(blue));
+        assertFalse(colors.contains(light_blue));
+    }
+
+    @Test
+    public void testGetCurrentColor(){
+        assertEquals(red, builder.getCurrentColor());
+        builder.switchColor(green);
+        assertEquals(green, builder.getCurrentColor());
+    }
+
+    @Test
+    public void testSetColor(){
+        assertEquals(green, builder.getColorAt(0, 0));
+    }
 }
--- src/test/java/edu/union/LevelImplTests.java
+++ src/test/java/edu/union/LevelImplTests.java
@@ -1,6 +1,8 @@
 package edu.union;
 
 import edu.union.model.*;
+import edu.union.service.LevelRepository;
+import edu.union.service.RawTextLevelRepositoryStrategy;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -8,20 +10,27 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class LevelImplTests {
-    private LevelImpl level;
-    private ColorGrid grid;
-    private Color color;
+    private Level level;
+    private RawTextLevelRepositoryStrategy repoStrat;
+
+    private final Color red = new Color(255, 0, 0);
+    private final Color green = new Color(0, 255, 0);
+    private final Color blue = new Color(0, 0, 255);
+    private final Color light_blue = new Color(0, 255, 255);
+
 
     @Before
     public void setUp(){
-        grid = new ColorGrid(5, 5);
-        level = new LevelImpl(grid, color, new ArrayList<Move>(){
-        }, new LevelInfo(1));
+        repoStrat = new RawTextLevelRepositoryStrategy();
+        repoStrat.setFolderPath("build/resources/test/edu.union/level");
+        LevelRepository.getInstance().setLevelRepositoryStrategy(repoStrat);
+        level = LevelRepository.getInstance().loadLevel(new LevelInfo(1));
     }
 
     @After
@@ -29,14 +38,130 @@ public class LevelImplTests {
 
     @Test
     public void testConstruct(){
-        System.out.println(level);
         assertEquals(5, level.getNumRows());
         assertEquals(5, level.getNumCols());
-        assertEquals(0, level.numMoveRemaining());
+        assertEquals(3, level.numMoveRemaining());
     }
 
-    //@Test(expected = RuntimeException.class)
-    //public void testConstruct_UnknownLevel(){
-    //    LevelImpl level = new LevelImpl(Integer.MAX_VALUE);
-    //}
+    @Test
+    public void testGetColorAt(){
+        for(int i = 0; i < 5; i++) {
+            assertEquals(red, level.getColorAt(0, i));
+        }
+
+        for (int i = 1; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                assertEquals(green, level.getColorAt(i, j));
+            }
+        }
+
+        for (int i = 1; i < 3; i++){
+            for (int j = 3; j < 5; j++){
+                assertEquals(blue, level.getColorAt(i, j));
+            }
+        }
+
+        for (int i = 3; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                assertEquals(light_blue, level.getColorAt(i, j));
+            }
+        }
+    }
+
+    @Test
+    public void testSwitchColor(){
+        level.switchColor(green);
+        assertEquals(green, level.getCurrentColor());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSwitchColor_Invalid(){
+        Color random = new Color(5, 5, 5);
+        level.switchColor(random);
+    }
+
+    @Test
+    public void testGetCurrentColor(){
+        assertEquals(red, level.getCurrentColor());
+
+        level.switchColor(green);
+        assertEquals(green, level.getCurrentColor());
+    }
+
+    @Test
+    public void testPlay(){
+        Move move = new Move(blue, 0, 0);
+        assertEquals(3, level.numMoveRemaining());
+        level.play(move);
+        for(int i = 0; i < 5; i++) {
+            assertEquals(blue, level.getColorAt(0, i));
+        }
+
+        assertEquals(2, level.numMoveRemaining());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testPlay_noMovesLeft(){
+        Move move1 = new Move(green, 0, 0);
+        level.play(move1);
+        Move move2 = new Move(red, 0, 1);
+        level.play(move2);
+        Move move3 = new Move(green, 0, 2);
+        level.play(move3);
+        Move move4 = new Move(red, 0, 3);
+        level.play(move4);
+    }
+
+    @Test
+    public void testGetColors(){
+        List<Color> colors = level.getColors();
+        assertTrue(colors.contains(red) && colors.contains(green)
+                && colors.contains(blue) && colors.contains(light_blue));
+    }
+
+    @Test
+    public void testGetHints(){
+        ArrayList<Move> hints = new ArrayList<>();
+        Move move1 = new Move(green, 0, 0);
+        Move move2 = new Move(green, 1, 3);
+        Move move3 = new Move(green, 4, 0);
+        hints.add(move1);
+        hints.add(move2);
+        hints.add(move3);
+
+        assertTrue(listsEqual(level.getHints(), hints));
+    }
+
+    @Test
+    public void testLevelState_Win(){
+        LevelState ongoing = LevelState.ONGOING;
+        LevelState win = LevelState.WIN;
+        assertEquals(ongoing, level.getLevelState());
+
+        List<Move> winningPlay = level.getHints();
+        int movesLeft = 3;
+        while(winningPlay.iterator().hasNext()){
+            Move move = winningPlay.get(0);
+            assertEquals(movesLeft, level.numMoveRemaining());
+            movesLeft--;
+            assertEquals(ongoing, level.getLevelState());
+            level.play(move);
+        }
+
+        assertEquals(win, level.getLevelState());
+    }
+
+
+    private boolean listsEqual(List<Move> hints, ArrayList<Move> otherHints){
+        for(int i =0; i < otherHints.size(); i++){
+            System.out.println(hints.get(i));
+            if(!hints.get(i).toString().equals((otherHints.get(0)).toString())){
+                return false;
+            }
+            hints.remove(hints.get(0));
+            if(hints.iterator().hasNext()){
+                return false;
+            }
+        }
+        return true;
+    }
 }
