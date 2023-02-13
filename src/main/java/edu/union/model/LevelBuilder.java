ec063bd19fc80fa118fbdc93d9c9118c17b386d3
Khai Dong <dongk@union.edu>
2023-02-13 18:45:12 -0500

    add method to levelBuilder to get the result level type

--- src/main/java/edu/union/model/LevelBuilder.java
+++ src/main/java/edu/union/model/LevelBuilder.java
@@ -18,6 +18,8 @@ public abstract class LevelBuilder <V extends ColoredVertex> extends Observable 
     protected ColoredGraph<V> graph;
     protected Color curColor;
 
+    public abstract String getType();
+
     /**
      * basic initialization calling Observable constructor
      */
