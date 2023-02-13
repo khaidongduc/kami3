39b8d7413fe5d4956ab8410d723def08a6469429
Khai Dong <dongk@union.edu>
2023-02-13 17:35:22 -0500

    implement abstract level with generic type

--- src/main/java/edu/union/model/Level.java
+++ src/main/java/edu/union/model/Level.java
@@ -1,57 +1,75 @@
 package edu.union.model;
 
+import edu.union.service.ColorRepository;
 import edu.union.utils.Observable;
+import edu.union.model.ColoredGraph.ColoredVertex;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * an abstract class defining all methods used in playing a level
  * it extend Observable to allows View to subscribe to its change
  */
-public abstract class Level extends Observable {
+public abstract class Level<V extends ColoredVertex> extends Observable {
+
+    protected LevelInfo levelInfo;
+    protected int curNumTurn;
+    protected List<Move<V>> hints;
+    protected Color curColor;
+    protected ColoredGraph<V> graph;
 
     /**
      * constructor
      * ensure to initialize Observable
      */
-    public Level() {
+    public Level(ColoredGraph<V> graph,
+                 Color curColor,
+                 List<Move<V>> hints,
+                 LevelInfo levelInfo) {
         super();
+        this.graph = graph;
+        this.curColor = curColor;
+        this.hints = hints;
+        this.levelInfo = levelInfo;
+        this.curNumTurn = 0;
     }
-
 
 
     /**
      * get the levelInfo
      * @return the levelInfo
      */
-    public abstract LevelInfo getLevelInfo();
+    public LevelInfo getLevelInfo(){
+        return levelInfo;
+    };
 
-    /**
-     * get the number of rows in the color grid associated with a level
-     * @return the number of rows in the color grid associated with a level
-     */
-    public abstract int getNumRows();
+    public Set<V> getVertexSet(){
+        return graph.getVertexSet();
+    }
 
-    /**
-     * get the number of columns in the color grid associated with a level
-     * @return the number of columns in the color grid associated with a level
-     */
-    public abstract int getNumCols();
+    public ColoredGraph<V> getGraph(){
+        return this.graph;
+    }
 
     /**
      * get the color at a specified cell
-     * @param row the row
-     * @param col the column
+     * @param vertex the vertex
      * @return the current color of that cell
      * @throws IllegalArgumentException if the index is out of bound
      */
-    public abstract Color getColorAt(int row, int col);
+    public Color getColorAt(V vertex){
+        int colorId = graph.getVertexColor(vertex);
+        return ColorRepository.getInstance().getColor(colorId);
+    }
 
     /**
      * get the number of moves remaining before losing the game
      * @return the number of moves remaining before losing the game
      */
-    public abstract int numMoveRemaining();
+    public int numMoveRemaining() {
+        return hints.size() - curNumTurn;
+    }
 
     /**
      * switch to a different color within the color palette of the grid
@@ -59,13 +77,18 @@ public abstract class Level extends Observable {
      * @param color the new color
      * @throws IllegalArgumentException if the color does not exist within palette, or it is the same color as the previous one
      */
-    public abstract void switchColor(Color color);
+    public void switchColor(Color color){
+        this.curColor = color;
+        notifyObservers();
+    }
 
     /**
      * get the current color that the level is using
      * @return the current color
      */
-    public abstract Color getCurrentColor();
+    public Color getCurrentColor(){
+        return this.curColor;
+    }
 
     /**
      * play a move on the level
@@ -75,25 +98,47 @@ public abstract class Level extends Observable {
      * @throws IllegalArgumentException if the color of the move is not in the grid
      * @throws IllegalArgumentException index out of bound
      */
-    public abstract void play(Move move);
+    public void play(Move<V> move){
+        if (numMoveRemaining() == 0){
+            throw new IllegalArgumentException("No move left");
+        }
+        graph.colorFloodFill(move.getColoredVertex(), move.getColor().getColorId());
+        ++curNumTurn;
+        notifyObservers();
+    }
 
     /**
      * get the color palette of this level
      * @return List of colors
      */
-    public abstract List<Color> getColors();
+    public Iterable<Color> getColors(){
+        return ColorRepository.getInstance().listColors(graph.getColorIds());
+    }
 
     /**
      * return hints to solve this level
      * @return the hints
      */
-    public abstract List<Move> getHints();
+    public Iterable<Move<V>> getHints(){
+        return hints;
+    }
 
     /**
      * get the current level's state
      * either ONGOING, WIN, LOSE
      * @return the level's current state
      */
-    public abstract LevelState getLevelState();
+    public LevelState getLevelState(){
+        boolean isMono = this.getGraph().pruneGraph().getNumVertices() == 1;
+        if(!isMono){
+            if(numMoveRemaining() > 0) {
+                return LevelState.ONGOING;
+            }
+            return LevelState.LOSE;
+        }
+        return LevelState.WIN;
+
+
+    }
 
 }
