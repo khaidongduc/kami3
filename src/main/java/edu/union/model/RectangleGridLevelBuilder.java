7e83a09b9904bcbfd8223f9632271040084b9c0a
Khai Dong <dongk@union.edu>
2023-02-13 19:26:34 -0500

    add a draft version of RectangleGridLevelBuilder. the actual one is being worked on by another person

---
+++ src/main/java/edu/union/model/RectangleGridLevelBuilder.java
@@ -0,0 +1,43 @@
+package edu.union.model;
+
+import java.awt.*;
+
+public class RectangleGridLevelBuilder extends LevelBuilder<RectangleGridCell>{
+
+    private int numRows;
+    private int numCols;
+
+    public final String TYPE = "Rectangle";
+    /**
+     * basic initialization calling Observable constructor
+     *
+     * @param graph A
+     */
+    public RectangleGridLevelBuilder(int rows, int cols) {
+        super(new ColoredGraph<RectangleGridCell>());
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                graph.addVertex(new RectangleGridCell(i,j),0);
+            }
+        }
+        graph.buildGraphWithAdjacency();
+
+        this.numRows = rows;
+        this.numCols = cols;
+
+    }
+
+    @Override
+    public String getType(){
+        return TYPE;
+    }
+
+    public int getRows(){
+        return this.numRows;
+    }
+
+    public int getCols(){
+        return this.numCols;
+    }
+
+}
