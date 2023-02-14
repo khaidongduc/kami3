36c3434a0175b0f56d7abd87ee52d984652ded98
Khai Dong <dongk@union.edu>
2023-02-14 17:08:39 -0500

    comment code

--- src/main/java/edu/union/service/LevelRepository.java
+++ src/main/java/edu/union/service/LevelRepository.java
@@ -2,13 +2,33 @@ package edu.union.service;
 
 import edu.union.model.*;
 
+/**
+ * abstract class for levelRepository
+ */
 public abstract class LevelRepository {
 
     protected LevelRepositoryStrategy strategy;
 
+    /**
+     * load level from a levelInfo
+     * will fail if the levelInfo of a wrong type is passed
+     * @param levelInfo the levelInfo
+     * @return the Level associated with levelInfo
+     */
     public abstract Level loadLevel(LevelInfo levelInfo);
+
+    /**
+     * save a level created by a levelBuilder to a specific folder
+     * will fail if the levelBuilder of a wrong type is passed
+     * @param levelBuilder the levelBuilder
+     * @param folderPath the folder path
+     */
     public abstract void saveLevel(LevelBuilder levelBuilder, String folderPath);
 
+    /**
+     * set the strategy for this LevelRepository
+     * @param strategy the strategy
+     */
     public void setLevelRepositoryStrategy(LevelRepositoryStrategy strategy){
         this.strategy = strategy;
     }
--- src/main/java/edu/union/service/LevelRepositoryStrategy.java
+++ src/main/java/edu/union/service/LevelRepositoryStrategy.java
@@ -4,9 +4,22 @@ import edu.union.model.Level;
 import edu.union.model.LevelBuilder;
 import edu.union.model.LevelInfo;
 
-import java.io.File;
-
+/**
+ * the strategy to deal with fileIO for levelRepository
+ */
 public interface LevelRepositoryStrategy {
+    /**
+     * load the levelInfo from a file
+     * will fail if the levelInfo is of the right type
+     * @param levelInfo the levelInfo
+     * @return a level
+     */
     Level loadFromFile(LevelInfo levelInfo);
+
+    /**
+     * save a level from a levelBuilder to a file in a specific folder
+     * @param levelBuilder the levelBuilder
+     * @param folderPath the folder path
+     */
     void saveToFile(LevelBuilder levelBuilder, String folderPath);
 }
