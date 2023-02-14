05f6e25d06859f81fd2e70172a8773ef0102351b
John Collins <collinsj@union.edu>
2023-02-14 12:13:05 -0500

    modifying the repository tests so they now test the LevelRepositoryManager class and are calling functions correctly

--- src/test/java/edu/union/LevelRepositoryManagerTests.java
+++ src/test/java/edu/union/LevelRepositoryManagerTests.java
@@ -1,8 +1,7 @@
 package edu.union;
 
 import edu.union.model.*;
-import edu.union.service.LevelRepository;
-import edu.union.service.RawTextLevelRepositoryStrategy;
+import edu.union.service.LevelRepositoryManager;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -15,28 +14,26 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
-public class LevelRepositoryStrategyTests {
-    private RawTextLevelRepositoryStrategy repoStrategy;
+public class LevelRepositoryManagerTests {
+    private LevelRepositoryManager levelRepositoryManager;
 
 
     @Before
     public void setUp(){
-        repoStrategy = new RawTextLevelRepositoryStrategy();
-        repoStrategy.setFolderPath("build/resources/test/edu.union/level");
+        levelRepositoryManager = LevelRepositoryManager.getInstance();
+        levelRepositoryManager.setFolderPath("build/resources/test/edu.union/level");
     }
 
     @After
-    public void tearDown(){repoStrategy = null;}
+    public void tearDown(){levelRepositoryManager = null;}
 
     @Test
     public void testLoad(){
-        LevelRepository.getInstance().setLevelRepositoryStrategy(repoStrategy);
-        Level level = LevelRepository.getInstance().loadLevel(new LevelInfo(1));
-        assertEquals(5, level.getNumRows());
-        assertEquals(5, level.getNumCols());
+        RectangleGridLevel level = (RectangleGridLevel) levelRepositoryManager.loadLevel(new LevelInfo(1, "RECTANGLE_GRID_LEVEL", "build/resources/test/edu.union/level"));
+        assertEquals(25, level.getGraph().getNumVertices());
         assertEquals(3, level.numMoveRemaining());
         assertEquals(new Color(255, 0, 0), level.getCurrentColor());
-        List<Move> hints = level.getHints();
+        List<Move<RectangleGridCell>> hints = level.getHints();
         for (Move m : hints){
             level.play(m);
         }
@@ -45,17 +42,17 @@ public class LevelRepositoryStrategyTests {
 
     @Test (expected = RuntimeException.class)
     public void testLoad_UnknownFile(){
-        repoStrategy.loadLevel(new LevelInfo(Integer.MAX_VALUE));
+        levelRepositoryManager.loadLevel(new LevelInfo(Integer.MAX_VALUE, "RECTANGLE_GRID_LEVEL", "build/resources/test/edu.union/level"));
     }
 
 
     @Test
     public void testSave(){
-        LevelBuilder builder = new LevelBuilderImpl();
-        builder.setColor(new Color(0, 255, 0), 0, 0);
-        repoStrategy.saveLevel(builder);
+        RectangleGridLevelBuilder builder = new RectangleGridLevelBuilder(5, 5);
+        builder.setColor(new Color(0, 255, 0), new RectangleGridCell(0, 0));
+        levelRepositoryManager.saveLevel(builder);
 
-        List<LevelInfo> levels = repoStrategy.listLevelInfo();
+        List<LevelInfo> levels = levelRepositoryManager.listLevelInfos();
         List<String> levelInfo = new ArrayList<>();
         for (LevelInfo l : levels){
             levelInfo.add(l.toString());
