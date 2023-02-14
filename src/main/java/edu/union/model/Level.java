4bd625a0483e8326dc9cd27bd4047eec5876f55c
Khai Dong <dongk@union.edu>
2023-02-14 16:03:21 -0500

    fix bug revealed by test testGetCurrentColor() in RectangleGridLevelTests

--- src/main/java/edu/union/model/Level.java
+++ src/main/java/edu/union/model/Level.java
@@ -3,7 +3,8 @@ package edu.union.model;
 import edu.union.service.ColorRepository;
 import edu.union.utils.Observable;
 import edu.union.model.ColoredGraph.ColoredVertex;
-
+import edu.union.model.Color;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
@@ -31,7 +32,8 @@ public abstract class Level<V extends ColoredVertex> extends Observable {
         this.hints = hints;
         this.levelInfo = levelInfo;
         this.curNumTurn = 0;
-        this.curColor = getColors().iterator().next(); // first color in the graph
+        this.curColor = getColors().stream().min(Comparator.comparingInt(Color::getColorId))
+                .get(); // first color in the graph
     }
 
     public abstract String getLevelType();
