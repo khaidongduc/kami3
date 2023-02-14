efb27d98050907c136de43635b737671a969e426
Khai Dong <dongk@union.edu>
2023-02-14 17:00:09 -0500

    comment levelBuilderFactory

--- src/main/java/edu/union/service/LevelBuilderFactory.java
+++ src/main/java/edu/union/service/LevelBuilderFactory.java
@@ -8,14 +8,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * general LevelBuilderFactory
+ * abstract the creation of LevelBuilder objects
+ * allow registering LevelBuilder prototype and clone that prototype everytime.
+ * is a singleton
+ */
 public class LevelBuilderFactory {
 
     private static LevelBuilderFactory instance;
-    private AbstractMap<String,LevelBuilder> map;
+    private AbstractMap<String, LevelBuilder> map;
     public LevelBuilderFactory(){
         map = new HashMap<>();
     }
 
+    /**
+     * get an instance of a levelBuilderFactory
+     * @return the instance
+     */
     public static LevelBuilderFactory getInstance(){
         if(instance == null){
             instance = new LevelBuilderFactory();
@@ -23,6 +33,12 @@ public class LevelBuilderFactory {
         return instance;
     }
 
+    /**
+     * create a level builder from the levelType
+     * @param levelType the levelType
+     * @return a levelBuilder of the right type
+     * @throws Exception if no prototype has been registered under this type
+     */
     public LevelBuilder createLevelBuilder(String levelType) throws Exception {
         if(map.containsKey(levelType)){
             return (LevelBuilder) map.get(levelType).clone();
@@ -30,6 +46,11 @@ public class LevelBuilderFactory {
         throw new Exception("We cannot find a LevelBuilder for the given key");
     }
 
+    /**
+     * register a prototype of a levelBuilder under a levelType
+     * @param levelType the levelType
+     * @param levelBuilder the levelBuilder prototype
+     */
     public void register(String levelType, LevelBuilder levelBuilder){
         map.put(levelType,levelBuilder);
     }
