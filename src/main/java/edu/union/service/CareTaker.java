69092d67cc18b261e7734f25a7b9e5ca1cdfe706
John Collins <collinsj@union.edu>
2023-02-27 16:55:17 -0500

    making the new caretaker class

---
+++ src/main/java/edu/union/service/CareTaker.java
@@ -0,0 +1,11 @@
+package edu.union.service;
+
+import java.util.Stack;
+
+public class CareTaker {
+    private Stack<Object> stack;
+
+    public CareTaker(){
+        stack = new Stack<>();
+    }
+}
